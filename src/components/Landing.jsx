7b20dc8a7754da6f0f1650e7e37c02e289e905fa
Jaytee <Padilla.Jaytee@gmail.com>
2020-03-24 22:34:36 -0700

    Progress commit. Currently working on auth0 implementation. The auth0 context has been setup

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,17 +1,17 @@
 import React, { useContext } from 'react';
 
 // Contexts
-import { Auth0Context } from '../contexts/auth0-context';
+// import { Auth0Context } from '../contexts/auth0-context';
 
 const Landing = () => {
   // auth0 context
-  const auth0 = useContext(Auth0Context);
+  // const auth0 = useContext(Auth0Context);
 
   return (
     <div className='hero is-info is-fullheight'>
       <div className='hero-body'>
         <div className='container has-text-centered is-size-1'>
-          {auth0.msg}
+          This is the landing page
         </div>
       </div>
     </div>
--- src/contexts/auth0-context.js
+++ src/contexts/auth0-context.js
@@ -1,24 +1,57 @@
-import React, { createContext, useState } from 'react'
+import React, { Component, createContext } from 'react';
+import createAuth0Client from '@auth0/auth0-spa-js';
 
-// create the context
+// create the context for auth0
 export const Auth0Context = createContext();
 
-// create a provider
-export const Auth0Provider = props => {
-  // creating the test message in state
-  const [state, setState] = useState({
-    msg: 'test message'
-  })
+// create the provider for auth0
+export class Auth0Provider extends Component {
+  // holds data that's used to track the processes of authenticating a user
+  state = {
+    auth0Client: null,
+    isLoading: true,
+    isAuthenticated: false,
+    user: null
+  };
 
-  const { children } = props;
+  // grabs the relevant data from .env
+  config = {
+    domain: process.env.REACT_APP_AUTH0_DOMAIN,
+    client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+    redirect_uri: window.location.origin
+  };
 
-  const configObject = state;
+  // when this components is mounted, stars the auth0 authentication process
+  componentDidMount() {
+    this.initializeAuth0();
+  }
 
-  return (
-    <div>
+  // initialize the auth0 library
+  initializeAuth0 = async () => {
+    const auth0Client = await createAuth0Client(this.config);
+    const isAuthenticated = await auth0Client.isAuthenticated();
+    const user = isAuthenticated ? await auth0Client.getUser(): null;
+
+    this.setState({auth0Client, isLoading: false, isAuthenticated, user});
+  };
+
+
+  render() {
+    // destructuring
+    const {isLoading, isAuthenticated, user} = this.state;
+    const {children} = this.props;
+
+    // contains info about current user's status of authentication
+    const configObject = {
+      isLoading,
+      isAuthenticated,
+      user,
+    };
+
+    return (
       <Auth0Context.Provider value={configObject}>
         {children}
       </Auth0Context.Provider>
-    </div>
-  )
+    );
+  }
 }
