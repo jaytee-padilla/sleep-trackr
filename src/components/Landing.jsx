a058393100118b1aa55348c24ae306128a571a6a
Jaytee <Padilla.Jaytee@gmail.com>
2020-03-26 00:09:03 -0700

    Added ability to login and logout via auth0

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,16 +5,37 @@ import { Auth0Context } from '../contexts/auth0-context';
 
 const Landing = () => {
   // auth0 context
-  const auth0 = useContext(Auth0Context);
+  const {isAuthenticated, user, loginWithRedirect, logout} = useContext(Auth0Context);
 
   return (
     <div className='hero is-info is-fullheight'>
       <div className='hero-body'>
         <div className='container has-text-centered'>
-          <h1 className='is-size-1'>Click Below!</h1>
-          <button onClick={auth0.loginWithRedirect} className='button is-danger'>
-            Login
-          </button>
+          {!isAuthenticated && !user && (
+            <>
+              <h1>Click Below!</h1>
+              <button onClick={loginWithRedirect} className='button is-danger'>
+                Login
+              </button>
+            </>
+          )}
+
+          {isAuthenticated && user && (
+            <>
+              <h1>You are logged in!</h1>
+              <p>Hello {user.name}</p>
+
+              {user.picture && <img src={user.picture} alt='My Avatar' />}
+              <hr />
+
+              <button
+                onClick={() => logout({ returnTo: window.location.origin })}
+                className="button is-small is-dark"
+              >
+                Logout
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
